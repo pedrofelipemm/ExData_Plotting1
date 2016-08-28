09e8bc9794d1ef4e852df67cebbb05c4fb4ff8a3
Pedro Felipe Marques Moreira <pedrofelipemm@gmail.com>
2016-08-28 09:20:24 -0300

    plot 1

---
+++ plot1.R
@@ -0,0 +1,6 @@
+### Assuming that you have the data file in your working directory ###
+data <- read.table("./household_power_consumption.txt", header = T, sep = ";", stringsAsFactors = F, dec = ".")
+data <- data[data$Date %in% c("1/2/2007", "2/2/2007") ,]
+png("plot1.png", width = 480, height = 480)
+hist(as.numeric(data$Global_active_power), col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
